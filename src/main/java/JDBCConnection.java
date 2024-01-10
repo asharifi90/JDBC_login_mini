8600276f25ec57affd0a67f72dc51fa8592a1aa1
asharifi90 <asharifibb@gmail.com>
2024-01-10 07:12:16 +0330

    add connection field and create a getter

--- src/main/java/JDBCConnection.java
+++ src/main/java/JDBCConnection.java
@@ -1,2 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnection {
+
+    private final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "BlackHead23");
+
+    public JDBCConnection() throws SQLException {
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
