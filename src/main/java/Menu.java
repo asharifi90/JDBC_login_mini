1a88202c993f0887c943b654f4851779f3e2fae6
asharifi90 <asharifibb@gmail.com>
2024-01-10 06:00:48 +0330

    add public menu

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    private final Scanner scanner = new Scanner(System.in);
+    public void publicMenu(){
+        System.out.println("*****WELLCOME*****");
+        System.out.println("1 -> Sign-up");
+        System.out.println("2 -> Sign-in");
+        System.out.println("3 -> Exit");
+        System.out.println("Choose your number");
+
+        int number = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println(number);
+    }
+
 }
