5d9021e0aa25c25293dbec41c5562c45712d5d6b
asharifi90 <asharifibb@gmail.com>
2024-01-10 06:33:58 +0330

    add signup metode to user service

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class UserService {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void signUp(){
+        System.out.println("Plz enter your firstname: ");
+        String firstname = scanner.nextLine();
+
+        System.out.println("Plz enter your lastname: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Plz enter your username: ");
+        String username = scanner.nextLine();
+
+        System.out.println("Plz enter your password: ");
+        String password = scanner.nextLine();
+
+        Users user = new Users(firstname, lastname, username, password);
+        System.out.println(user);
+    }
 }
